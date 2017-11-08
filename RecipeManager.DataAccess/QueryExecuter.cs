d0598d9244425cf9e19365f740cffbbd3c4b6996
Benjamin Joshua Bridgens <benj4183@edu.campusvejle.dk>
2017-11-08 11:30:51 +0100

    Added Functionality to QueryExecuter.

--- RecipeManager.DataAccess/QueryExecuter.cs
+++ RecipeManager.DataAccess/QueryExecuter.cs
@@ -3,11 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace RecipeManager.DataAccess
 {
     public class QueryExecuter
     {
-        
+        #region Constants
+        private const string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=RecipeManagerDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        #endregion
+
+        #region Constructors
+        public QueryExecuter()
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+                connection.Close();
+            }
+            catch( Exception )
+            {
+                throw;
+            }
+        }
+        #endregion
+
+        #region Methods
+        public DataSet Execute(string sql)
+        {
+            try
+            {
+                DataSet resultSet = new DataSet();
+                using( SqlDataAdapter adapter = new SqlDataAdapter(new SqlCommand(sql, new SqlConnection(connectionString))) )
+                {
+                    adapter.Fill(resultSet);
+                }
+                return resultSet;
+            }
+            catch( Exception )
+            {
+                throw;
+            }
+        }
+        #endregion
     }
 }
