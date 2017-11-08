eab8445a17bf31d47ff6d5f14160df69c463cb16
Benjamin Joshua Bridgens <benj4183@edu.campusvejle.dk>
2017-11-08 11:52:02 +0100

    Added Fields, Constructors and Properties to the Recipe class.

--- RecipeManager.Entities/Recipe.cs
+++ RecipeManager.Entities/Recipe.cs
@@ -7,6 +7,31 @@ using System.Threading.Tasks;
 namespace RecipeManager.Entities
 {
     public class Recipe
-    {        
+    {
+        #region Fields
+        private string name;
+        private List<Ingredient> ingredients;
+        private int recipeID;
+        #endregion
+
+        #region Constructors
+        public Recipe(string name, List<Ingredient> ingredints, int recipeID)
+        {
+            Name = name;
+            Ingredients = ingredients;
+            RecipeID = recipeID;
+        }
+        #endregion
+
+        #region Properties
+        public string Name { get => name; set => name = value; }
+        public int RecipeID { get => recipeID; set => recipeID = value; }
+        public List<Ingredient> Ingredients { get => ingredients; set => ingredients = value; }
+        #endregion
+
+        #region Methods
+
+        #endregion
+
     }
 }
