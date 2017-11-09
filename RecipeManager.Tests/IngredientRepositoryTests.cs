19d46ef655d3a0406b7ed252aad5e301e111dd9c
Benjamin Joshua Bridgens <benj4183@edu.campusvejle.dk>
2017-11-09 13:15:36 +0100

    Added GetAllIngredientsPasses

---
+++ RecipeManager.Tests/IngredientRepositoryTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RecipeManager.DataAccess;
+using RecipeManager.Entities;
+using System.Collections.Generic;
+
+namespace RecipeManager.Tests
+{
+    [TestClass]
+    public class IngredientRepositoryTests
+    {
+        [TestMethod]
+        public void GetAllIngredientsPasses()
+        {
+            // Arrange:
+            bool expected = true;
+            bool actual;
+            IngredientRepository ingRepo = new IngredientRepository();
+
+            // Act:
+            List<Ingredient> ingredients = ingRepo.GetAllIngredients();
+
+            // Assert:
+            if(ingredients.Count > 0 )
+            {
+                actual = true;
+            }
+            else
+            {
+                actual = false;
+            }
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
