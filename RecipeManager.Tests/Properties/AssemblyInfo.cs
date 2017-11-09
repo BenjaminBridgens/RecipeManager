cadd9e15c9608ca427c346f2740fb8d087579a19
Benjamin Joshua Bridgens <benj4183@edu.campusvejle.dk>
2017-11-09 11:37:31 +0100

    Added RecipeManager.Tests and QueryExecutorTests

---
+++ RecipeManager.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RecipeManager.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RecipeManager.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a140ebf3-9e3f-4b0c-b242-076780a29b33")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RecipeManager.Tests/QueryExecutorTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RecipeManager.DataAccess;
+using System.Data;
+
+namespace RecipeManager.Tests
+{
+    [TestClass]
+    public class QueryExecutorTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
