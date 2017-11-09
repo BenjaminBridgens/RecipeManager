bceb949c3e608ef69f55dc1fe3e37adb152d3275
Benjamin Joshua Bridgens <benj4183@edu.campusvejle.dk>
2017-11-09 10:32:12 +0100

    Added GetAllIngredients

---
+++ RecipeManager.DataAccess/IngredientRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RecipeManager.Entities;
+using System.Data;
+
+namespace RecipeManager.DataAccess
+{
+    public class IngredientRepository: DataRepository
+    {
+        public List<Ingredient> GetAllIngredients()
+        {
+            string sql = $"SELECT * FROM Ingredients";
+            DataSet set = executer.Execute(sql);
+            List<Ingredient> allIngredients = new List<Ingredient>(0);
+            DataTable table = set.Tables[0];
+            foreach(DataRow row in table.Rows )
+            {
+                int id = (int)row["IngredientId"];
+                string name = (string)row["IngredientName"];
+                decimal price = (decimal)row["Price"];
+                string kind = (string)row["IngredientType"];
+                IngredientKind iK = (IngredientKind)Enum.Parse(typeof(IngredientKind), ((string)row["IngredientType"]).Trim());
+                Ingredient i = new Ingredient(price, name, iK, id);
+
+                allIngredients.Add(i);
+            }
+
+            return allIngredients;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
